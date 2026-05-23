86ca110b2ce9c4cce69e5b66622c9286e1b8b0ac
sean22-blip <iseandxd@gmail.com>
2026-05-23 18:23:19 +0700

    implementing the logger.js

--- StartCode/StartCode/Ex3/logger.js
+++ StartCode/StartCode/Ex3/logger.js
@@ -1,38 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8000;
-app.get("/", ( req, res, next) => {
-console.log("first middleware");
-setTimeout(() => {
-
-    next();
-}, 2000);
+// app.use(express.json());
+function login(req, res, next) {
+  const method = req.method;
+  const url = req.url;
+  console.log(`Method: ${method} + URL: ${url}`);
+  next();
 }
-)
-app.get("/", ( req, res, next) => {
-console.log("second middleware");
-setTimeout(() => {
-
-    next();
-}, 2000);
-}
-)
-app.get("/", ( req, res, next) => {
-console.log("third middleware");
-setTimeout(() => {
-
-    next();
-}, 2000);
-}
-)
-app.get("/", ( req, res, next) => {
-console.log("fourth middleware");
-setTimeout(() => {
-    console.log("Finished the middleware!");
-    res.send("Finished the middleware");
-}, 2000);
-}
-)
-app.listen(port, () => {
-    console.log(`server is running at ${port}`)
+app.use(login);
+app.get("/", (req, res, next) => {
+  res.send(`<h1>This is the logger page!</h1>`);
 });
+app.post("/user", (req, res, next) => {
+  const { method, url } = req;
+  res.send(
+    `<h1>End of the system thank you ${method} + for using ${url} !!</h1>`,
+  );
+});
+// 1. request to the server so uses get
+// 2. send respond or pass to the next middlware if no middlware then end the cycle
+app.listen(port, () => console.log(`server is running at ${port}`));
