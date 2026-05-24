5fdba6020cfa8f7a73564be78ddc1ea5a3223162
sean22-blip <iseandxd@gmail.com>
2026-05-24 12:33:36 +0700

    doing logger and validating Query

--- StartCode/StartCode/Ex3/server.js
+++ StartCode/StartCode/Ex3/server.js
@@ -1,3 +1,8 @@
 const express = require('express');
+const logger = require('./logger');
+const validating = require('./validateQuery');
+const courses = require('../EX-2/course');
+// const { log } = require('node:console');
 const app = express();
 const port = 8000;
+app.use(logger);
--- StartCode/StartCode/Ex3/validateQuery.js
+++ StartCode/StartCode/Ex3/validateQuery.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+
+
+function validating(req, res, next){
+    const {minCredits, maxCredits} = req.query;
+if(minCredits !== undefined &&  maxCredits != undefined){
+    console.log("Valid!");
+}
+if(minCredits > maxCredits){
+    return res.status(400).json({error: "404 error!"})
+}
+next();
+}
+module.exports = validating;
